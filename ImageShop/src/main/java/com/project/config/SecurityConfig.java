91d7387ab3e2c1dfa96fcb0877f74db964d2e00b
kdongjin <rlaeogus0911@nate.com>
2025-01-22 17:54:14 +0900

    스프링부트 이미삽 회원게시판 테스팅

--- ImageShop/src/main/java/com/project/config/SecurityConfig.java
+++ ImageShop/src/main/java/com/project/config/SecurityConfig.java
@@ -26,75 +26,77 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+//1. 시큐리티 설정한다.
 @EnableWebSecurity
+//2. 시큐리티 애너테이션 활성화를 위한 설정 
+@EnableMethodSecurity(prePostEnabled=true, securedEnabled=true)
 public class SecurityConfig {
-	//@Autowired
-	//DataSource dataSource;
+	//2. 데이타정보 주입
+	@Autowired
+	DataSource dataSource;
 
+	//3. 시큐리티 설정관리 함수
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		// csrf 토큰 비활성화
+		//4.csrf 토큰 비활성화
 		http.csrf().disable();
 		// URI 패턴으로 접근 제한을 설정한다.
-		// 인가설정
-		http.authorizeRequests().requestMatchers("/board/**").authenticated();
-		http.authorizeRequests().requestMatchers("/manager/**").hasRole("MANAGER");
-		http.authorizeRequests().requestMatchers("/admin/**").hasRole("ADMIN");
-		http.authorizeRequests().anyRequest().permitAll();
+		//5. 인가설정(모든팀원이 알아서 결정)
+		//http.authorizeRequests().requestMatchers("/board/**").authenticated();
+		//http.authorizeRequests().requestMatchers("/manager/**").hasRole("MANAGER");
+		//http.authorizeRequests().requestMatchers("/admin/**").hasRole("ADMIN");
+		//http.authorizeRequests().anyRequest().permitAll();
 		
-		// 개발자가 정의한 로그인 페이지의 URI를 지정한다.
-		// 로그인 성공 후 처리를 담당하는 처리자로 지정한다.
-		http.formLogin(); 
-		//http.formLogin().loginPage("/login").successHandler(createAuthenticationSuccessHandler());
-		// 로그아웃 처리를 위한 URI를 지정하고, 로그아웃한 후에 세션을 무효화 한다.
-		//http.logout().logoutUrl("/logout").invalidateHttpSession(true).deleteCookies("remember-me", "JSESSION_ID");
-		// 등록한 CustomAccessDeniedHandler를 접근 거부 처리자로 지정한다.
-		//http.exceptionHandling().accessDeniedHandler(createAccessDeniedHandler());
+		//6. 개발자가 정의한 로그인 페이지의 URI를 지정하고 로그인 성공 후 처리를 담당하는 핸들러 콜한다.
+		http.formLogin().loginPage("/auth/login").loginProcessingUrl("/login").
+		successHandler(createAuthenticationSuccessHandler());
 
-		// 데이터 소스를 지정하고 테이블을 이용해서 기존 로그인 정보를 기록
-		// 쿠키의 유효 시간을 지정한다(24시간).
-		//http.rememberMe().key("zeus").tokenRepository(createJDBCRepository()).tokenValiditySeconds(60 * 60 * 24);
+		//7. 로그아웃 처리를 위한 URI를 지정하고, 로그아웃한 후에 세션을 무효화 한다.
+		// 로그아웃을 하면 자동 로그인에 사용하는 쿠키도 삭제한다
+		http.logout().logoutUrl("/auth/logout").invalidateHttpSession(true).deleteCookies("remember-me", "JSESSION_ID");
+
+		//8.로그인 실패시 CustomAccessDeniedHandler를 접근 거부 처리자로 지정한다.
+		http.exceptionHandling().accessDeniedHandler(createAccessDeniedHandler());
+
+		//9. 데이터 소스를 지정하고 테이블을 이용해서 기존 로그인 정보를 기록 쿠키의 유효 시간을 지정한다(24시간).
+		http.rememberMe().key("zeus").tokenRepository(createJDBCRepository())
+		.tokenValiditySeconds(60 * 60 * 24);
 
 		return http.build();
 
 	}
 
-	/*
+	//1. read me
 	private PersistentTokenRepository createJDBCRepository() {
 		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
 		repo.setDataSource(dataSource);
 		return repo;
 	}
-    */
+	
+	//2. 데이터베이스를 통한 회원인증관리
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(createUserDetailsService()).passwordEncoder(createPasswordEncoder());
 	}
 
-	// 스프링 시큐리티의 UserDetailsService를 구현한 클래스를 빈으로 등록한다.
+	//3. 스프링 시큐리티의 UserDetailsService를 구현한 클래스를 빈으로 등록한다.
 	@Bean
 	public UserDetailsService createUserDetailsService() {
 		return new CustomUserDetailsService();
 	}
-
-	// 사용자가 정의한 비번 암호화 처리기를 빈으로 등록한다.
-	/*
-	@Bean
-	public PasswordEncoder createPasswordEncoder() {
-		return new CustomNoOpPasswordEncoder();
-	}
-	*/
-	
+		
+	//4. 사용자가 정의한 비번 암호화 처리기를 빈으로 등록한다
 	@Bean
 	public PasswordEncoder createPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	// CustomAccessDeniedHandler를 빈으로 등록한다.
+	//5. 로그인 잘못되었을때 발생되는 핸들러 처리 CustomAccessDeniedHandler를 빈으로 등록한다.
 	@Bean
 	public AccessDeniedHandler createAccessDeniedHandler() {
 		return new CustomAccessDeniedHandler();
 	}
 
+	//6. 로그인 성공되었을때 발생되는 핸들러 처리
 	@Bean
 	public AuthenticationSuccessHandler createAuthenticationSuccessHandler() {
 		return new CustomLoginSuccessHandler();
--- ImageShop/src/main/webapp/WEB-INF/views/common/header.jsp
+++ ImageShop/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -1,13 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
 <div align="right">
 	<table>
 		<tr>
-			<td width="80">
-				<a href="/user/register"> <spring:message code="header.joinMember"/></a>
-			</td>
+			<!-- 로그인 하지 않은 경우  -->
+			<sec:authorize access="!isAuthenticated()">
+				<td width="80"><a href="/user/register"><spring:message code="header.joinMember" /></a></td>
+				<td width="80"><a href="/auth/login"><spring:message code="header.login" /></a></td>
+			</sec:authorize>
+			<!-- 로그인 인증된 사용자 -->
+			<sec:authorize access="isAuthenticated()">
+				<td width="180"><sec:authentication property="principal.username" /> 님 <a href="/auth/logout"> <spring:message code="header.logout" /></a></td>
+			</sec:authorize>
 		</tr>
 	</table>
 </div>
+
 <hr>
 
